78f60d232186696f313ab48858aeaedea1ffea9c
George Kalandadze <kalandadzeg777@gmail.com>
2023-11-12 00:59:30 +0400

    feat:make adding of dynamic form for education page

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext } from "react";
+import { useSessionStorage } from "../Hooks/useSessionStorage";
 
 
 const AppContext = createContext({});
@@ -31,12 +32,46 @@ const resumeInfo = {
 
 
 export const AppProvider = ({children}) => {
+    const [info, setInfo] = useSessionStorage("resume-info", resumeInfo);
+      
+
+    const handleAddClick = (type) => {
+      if (type === "experience") {
+        setInfo({
+          ...info,
+          experiences: [
+            ...info.experiences,
+            {
+              position: "",
+              employer: "",
+              start_date: "",
+              due_date: "",
+              description: "",
+            },
+          ],
+        });
+      } else if (type === "education") {
+        setInfo({
+          ...info,
+          educations: [
+            ...info.educations,
+            { institute: "", due_date: "", description: "", degree_id: "" },
+          ],
+        });
+      }
+    };
 
 
-
-    return <AppContext.Provider>
+    return (
+      <AppContext.Provider
+        value={{
+          info,
+          handleAddClick
+        }}
+      >
         {children}
-    </AppContext.Provider>
+      </AppContext.Provider>
+    );
 
 }
 
--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 import InputGroup from '../../Components/InputGroup';
 import TextareaGroup from '../../Components/TextareaGroup';
+import { useGlobalContext } from '../../Context/Context';
 
 const Education = () => {
+  const { info, handleAddClick} = useGlobalContext();
   return (
     <div className="bg-white px-[200px] py-[20px]">
       <div className=" bg-[#f9f9f9] py-[20px] px-[30px] w-full h-full rounded flex flex-col gap-8">
-        <InputGroup
-          name=""
-          label="სასწავლებელი"
-          placeholder="სასწავლებელი"
-          error="მინიმუმ 2 სიმბოლო"
-        />
+        {info.educations.map((x, i) => (
+          <div className="flex flex-col gap-8">
+            <InputGroup
+              name=""
+              label="სასწავლებელი"
+              placeholder="სასწავლებელი"
+              error="მინიმუმ 2 სიმბოლო"
+            />
 
-        <InputGroup type="date" name="start_date" label="დაწყების რიცხვი" />
-        <InputGroup type="date" name="end_date" label="დამთავრების რიცხვი" />
-        <TextareaGroup
-          label="აღწერა"
-          placeholder="როლი თანამდებობაზე და ზოგადი აღწერა"
-        />
+            <InputGroup type="date" name="start_date" label="დაწყების რიცხვი" />
+            <InputGroup
+              type="date"
+              name="end_date"
+              label="დამთავრების რიცხვი"
+            />
+            <TextareaGroup
+              label="აღწერა"
+              placeholder="როლი თანამდებობაზე და ზოგადი აღწერა"
+            />
+          </div>
+        ))}
         <div className="flex justify-start">
-          <button className="bg-[#62a1eb] rounded text-white px-4 py-3">
+          <button
+            className="bg-[#62a1eb] rounded text-white px-4 py-3"
+            onClick={() => handleAddClick("education")}
+          >
             მეტი გამოცდილების დამატება
           </button>
         </div>
